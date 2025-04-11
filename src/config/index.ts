58cd8a62eccf57702a4cf15a9b9bba7b2691718f
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2025-04-11 16:36:10 +0700

    Create index.ts for const config data.

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+import minioConfig from './minio.config';
+import tokenConfig from './token.config';
+import mongoConfig from './mongo.config';
+import smsConfig from './sms.config';
+export default [minioConfig, tokenConfig, mongoConfig, smsConfig];
