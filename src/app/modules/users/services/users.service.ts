b175429d2029c930983a6f94c57c2189da49d2c3
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2025-04-11 16:35:43 +0700

    Add method for profile image upload.

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from '../query/users.repository';
 import { User } from 'src/database/mongodb/schemas/User.schema';
-import { ResourceNotFoundException } from 'src/core/exceptions/ResourceNotFoundException';
+import { ResourceNotFoundException } from 'src/core/exceptions/ResourceNotFound.exception';
+import { UploadService } from '../../upload/services/upload.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly uploadService: UploadService,
+  ) {}
 
   async insert(data: User): Promise<User> {
     return await this.usersRepository.insert(data);
@@ -14,7 +18,7 @@ export class UsersService {
   async update(data: User): Promise<User> {
     const user: User = await this.usersRepository.update(data);
 
-    if (user === undefined) {
+    if (user === null) {
       throw new ResourceNotFoundException('User not found');
     }
     return user;
@@ -23,7 +27,7 @@ export class UsersService {
   async getById(id: string): Promise<User> {
     const user: User = await this.usersRepository.getById(id);
 
-    if (user === undefined) {
+    if (user === null) {
       throw new ResourceNotFoundException('User not found');
     }
     return user;
@@ -32,7 +36,7 @@ export class UsersService {
   async getByPhone(phoneNumber: string): Promise<User> {
     const user: User = await this.usersRepository.getByPhone(phoneNumber);
 
-    if (user === undefined) {
+    if (user === null) {
       throw new ResourceNotFoundException('User not found');
     }
     return user;
@@ -41,7 +45,7 @@ export class UsersService {
   async updateRefreshTokenById(id: string, token: string): Promise<User> {
     const user: User | undefined =
       await this.usersRepository.updateRefreshTokenById(id, token);
-    if (user === undefined) {
+    if (user === null) {
       throw new ResourceNotFoundException('User not found');
     }
     return user;
@@ -50,9 +54,24 @@ export class UsersService {
   async updateRefreshTokenByPhone(phone: string, token: string): Promise<User> {
     const user: User | undefined =
       await this.usersRepository.updateRefreshTokenByPhone(phone, token);
-    if (user === undefined) {
+    if (user === null) {
       throw new ResourceNotFoundException('User not found');
     }
     return user;
+  }
+
+  async profileUpload(data: User, file: Express.Multer.File) {
+    const saveFileUUID: string = await this.uploadService.upload(
+      String(data._id),
+      file,
+    );
+
+    data.profilePic = saveFileUUID;
+
+    const user: User = await this.usersRepository.update(data);
+
+    if (user === null) {
+      throw new ResourceNotFoundException('User not found');
+    }
   }
 }
