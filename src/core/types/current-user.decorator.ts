eea3e91fe916a7ebfdc11fb5fab013db04a75a3b
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2025-04-11 16:38:41 +0700

    Refactory

--- src/core/types/current-user.decorator.ts
+++ src/core/types/current-user.decorator.ts
@@ -1,9 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/database/mongodb/schemas/User.schema';
 
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) =>
+  (_data: string, context: ExecutionContext) =>
     getCurrentUserByContext(context),
 );
 
-const getCurrentUserByContext = (context: ExecutionContext) =>
-  context.switchToHttp().getRequest().user;
+const getCurrentUserByContext = (context: ExecutionContext) => {
+  return context.switchToHttp().getRequest<{ user: User }>().user;
+};
