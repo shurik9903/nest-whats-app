ded6cdca6e99c40357667b3eff4d8e90682a2c6f
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2025-04-11 16:30:20 +0700

    Add Minio, Upload module. Refactory.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
+import { MongodbModule } from './../database/mongodb/mongodb.module';
+import { MinioModule } from './../database/minio/minio.module';
+import { UploadModule } from './modules/upload/upload.module';
 import { CoreModule } from './../core/core.module';
 import { UsersModule } from './modules/users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import config from 'src/config';
 
 @Module({
   imports: [
+    MongodbModule,
+    MinioModule,
+    UploadModule,
     CoreModule,
     AuthModule,
     UsersModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'),
-        pass: config.get<string>('MONGO_PASSWORD'),
-        user: config.get<string>('MONGO_USER'),
-        authSource: 'admin',
-      }),
-    }),
+    ConfigModule.forRoot({ isGlobal: true, load: [...config] }),
   ],
   controllers: [AppController],
   providers: [AppService],
